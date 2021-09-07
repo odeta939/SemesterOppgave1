c6453346242975d06f06117571f3f53e3ab93d70
odeta <odeta@disputas.no>
2021-09-07 15:16:29 +0200

    intial setup

---
+++ SemesterOppgave1/SemesterOppgave1/Controllers/Controller.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SemesterOppgave1.Controllers
+{
+    public class Controller
+    {
+    }
+}
---
+++ SemesterOppgave1/SemesterOppgave1/Models/Model.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SemesterOppgave1.Models
+{
+    public class Model
+    {
+    }
+}
