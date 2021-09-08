20b435df37d765397e4ce48858ac93e0b9b622e5
mbaan2 <mbaan2@gmail.com>
2021-09-08 17:59:23 +0200

    pop uå working :)

---
+++ SemesterOppgave1/SemesterOppgave1/wwwroot/js/orderConfirmation.js
@@ -0,0 +1,17 @@
+﻿$(document).ready(function () {
+    $("#buyTicket").click(function () {
+        $("#popUp").css("display", "block");
+    });
+    $("#popUp-content-close").click(function () {
+        $("#popUp").css("display", "none");
+    });
+
+    $(window).click(function (element) {
+        if (element.target.id == "popUp") {
+            $("#popUp").css("display", "none");
+        }
+        //alert(e.target.id); // gives the element's ID 
+        //alert(e.target.className); // gives the elements class(es)
+    });
+
+});
