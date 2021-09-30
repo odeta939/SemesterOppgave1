1e61895d9e3027a7924c8897d41a435e184e2304
odeta <odeta@disputas.no>
2021-09-30 15:24:17 +0200

    calendar get days coulourful :3

--- SemesterOppgave1/SemesterOppgave1/wwwroot/js/calendar.js
+++ SemesterOppgave1/SemesterOppgave1/wwwroot/js/calendar.js
@@ -1,43 +1,46 @@
-﻿$(function () {
-    let d = new Date();
-    let today = d.getDay() + "-" + d.getMonth() + "-" + d.getFullYear();
-   // console.log(today)
+﻿/*
+ * https://github.com/beinoriusju/TavoCalendar
+ * documentation for the calendar
+ * 
+ * */
+
+
+$(function () {
+
+    getDepartureDates()
+   
     let myCalendar = new TavoCalendar('#my-calendar', options)
-    available()
+   
     
-   // console.log(getDepartureDates())
 });
 
 function getDepartureDates() {
     //save departure times
-    const departureTimes = []
+    let departureTimes = []
     //get all routes
     $.get("order/GetAllRoutes", function (routes) {
         //get departure time for each route
         $.each(routes, function (index, value) {
             //add the time to array
-        departureTimes.push(value.departureTime)
+            
+            departureTimes.push(value.departureTime)
         });
+    }).then(() => {
+        available(departureTimes)
     });
-
     return departureTimes
 }
 
 var options = {
     format: 'DD-MM-YYYY',
-    highlight: ['12-10-2021']
-   
+    highlight: ['12-10-2021'],
+    blacklist: ['14-10-2021']
 }
-function available() {
-    //get all departure dates
-    const dates = getDepartureDates()
 
-    for (var i = 0; i < dates.length; i++) {
-        options.highlight.push(dates[i])
-        console.log(i)
-
-    }
-   /* $.each(dates, function (index, value) {
-        
-    });*/
-};
+function available(dates) {
+    console.log( dates)
+    $.each(dates, function (index, value) {
+        options.highlight.push(value)
+    
+    });
+}
