6a2a6cd4ee39ce66a76c73e18676559b66b40721
mbaan2 <mbaan2@gmail.com>
2021-09-30 16:09:43 +0200

    select auto

---
+++ SemesterOppgave1/SemesterOppgave1/wwwroot/js/routeDataLoader.js
@@ -0,0 +1,61 @@
+﻿$(function () {
+    const selectTo = $("#toPlace");
+    const arrayPlaces = getDeparturePlaces();
+    console.log(arrayPlaces);
+    console.log(arrayPlaces[0])
+
+    for (var i = 0; i < arrayPlaces.length; i++) {
+        console.log(arrayPlaces[i])
+    }
+
+
+    /*arrayPlaces.each(function (place) {
+        console.log(place);
+
+    });*/
+
+
+
+    /*
+    selectTo.append($('<option>', {
+        value: "Oslo",
+        text: "Oslo"
+    }));
+    selectTo.append($('<option>', {
+        value: "Kiel",
+        text: "Kiel"
+    }));
+    selectTo.append($('<option>', {
+        value: "Koben",
+        text: "Koben"
+    }));
+    selectTo.append($('<option>', {
+        value: "Gøteborg",
+        text: "Gøteborg"
+    }));*/
+
+
+    const selectFrom = $("fromPlace");
+
+
+
+});
+
+
+function getDeparturePlaces() {
+
+    //save departure times
+    const departurePlaces = []
+    //get all routes
+    $.get("order/GetAllRoutes", function (routes) {
+        //get departure time for each route
+        $.each(routes, function (index, value) {
+            const place = value.departureTerminalCity;
+
+            if (departurePlaces.indexOf(place) == -1) {
+                departurePlaces.push(place)
+            }
+        });
+    });
+    return departurePlaces
+}
